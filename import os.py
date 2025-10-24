3df98949ddea72d4a3179c2eaa228bcda0ce0d34
Sibasman <toloknov.2008@gmail.com>
2025-10-24 21:24:47 +0300

    кастыли шарят

--- import os.py
+++ import os.py
@@ -3,7 +3,6 @@ import random
 matrix_x = 4
 matrix_y = 4
 
-# Создание поля
 matrix = []
 for i_x in range(matrix_x):
     matrix_row = []
@@ -11,16 +10,13 @@ for i_x in range(matrix_x):
         matrix_row.append(random.randrange(0, 6, 2))
     matrix.append(matrix_row)
 
-# Основной цикл
 while True:
-    # заменя 0 на . и уберания слостных " " для упрощения визуала
+    
     for row in matrix:
         print(' '.join(str(val) if val != 0 else '.' for val in row))
-
-    # Ввод хода
+        
     input_keybord = input("Ход (w/a/s/d): ").lower()
 
-    # Движение вверх (w)
     if input_keybord == "w":
         for y in range(4):
             for x in range(1, 4):
@@ -37,7 +33,6 @@ while True:
                         else:
                             break
 
-    # Движение вниз (s)
     elif input_keybord == "s":
         for y in range(4):
             for x in range(2, -1, -1):
@@ -54,7 +49,6 @@ while True:
                         else:
                             break
 
-    # Движение влево (a)
     elif input_keybord == "a":
         for x in range(4):
             for y in range(1, 4):
@@ -71,7 +65,6 @@ while True:
                         else:
                             break
 
-    # Движение вправо (d)
     elif input_keybord == "d":
         for x in range(4):
             for y in range(2, -1, -1):
@@ -91,10 +84,7 @@ while True:
         print("Игра приостановленна")
         break
     
-    # Добавление новой плитки после хода
     empty = [(x, y) for x in range(4) for y in range(4) if matrix[x][y] == 0]
     if empty:
         x, y = random.choice(empty)
         matrix[x][y] = random.choice([2, 4])
-
-#коменты писал сам, печеньками не кидайтесь, это для себя сейвить буду . 
